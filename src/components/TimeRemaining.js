59550f3fba3f0ae0fb5ca330d940c917daf25b56
Ludmila Antuofermo <l.antuofermo@web.de>
2020-03-17 14:00:31 +0100

    Change hours remaining logic

--- src/components/TimeRemaining.js
+++ src/components/TimeRemaining.js
@@ -11,8 +11,15 @@ export default function TimeRemaining({ activities }) {
       {remainingHours(activities) === 1 ? 'hour' : 'hours'} to add to your day,
       use {remainingHours(activities) === 1 ? 'it' : 'them'} wisely!
     </TimeLeftStyled>
+  ) : remainingHours(activities) === 0 ? (
+    <NoTimeLeftStyled>
+      Day full! You don't have any hours remaining.
+    </NoTimeLeftStyled>
   ) : (
-    <TimeLeftStyled>You don't have any hours remaining</TimeLeftStyled>
+    <RemovehoursStyled>
+      You added too many hours to your day! Remove {hourOverflow(activities)}{' '}
+      hours or more before adding a new activity.
+    </RemovehoursStyled>
   )
 
   function remainingHours(activities) {
@@ -21,10 +28,39 @@ export default function TimeRemaining({ activities }) {
     }, 0)
     return 24 - sum
   }
+
+  function hourOverflow(activities) {
+    const sum = activities.reduce((sum, activityItem) => {
+      return sum + activityItem.hours
+    }, 0)
+    return sum - 24 + 1
+  }
 }
 
 const TimeLeftStyled = styled.p`
   margin-top: 50px;
   text-align: center;
   margin: 30px;
+  user-select: none;
+`
+
+const NoTimeLeftStyled = styled.p`
+  margin-top: 50px;
+  text-align: center;
+  margin: 30px;
+  color: black;
+  user-select: none;
+`
+
+const RemovehoursStyled = styled.div`
+  padding: 15px;
+  border-radius: 4px;
+  position: relative;
+  z-index: 1;
+  background: black;
+  margin-top: 50px;
+  text-align: center;
+  margin: 30px;
+  color: #b20238;
+  user-select: none;
 `
