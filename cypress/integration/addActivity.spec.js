ddc02a01e21e06822f5613805cab8af646e6448d
Ludmila Antuofermo <l.antuofermo@web.de>
2020-03-20 17:19:08 +0100

    Add addactivity cypress test

--- cypress/integration/addActivity.spec.js
+++ cypress/integration/addActivity.spec.js
@@ -1,21 +1,31 @@
 describe('Add new activity', () => {
-  beforeEach(() => {
+  it('opens the form', () => {
     cy.visit('http://localhost:3000')
-
     cy.get('main button').click()
+  })
+
+  it('fills in the form', () => {
+    cy.get('form button').should('disabled')
+    cy.get('input[name=name]').should('be.focused')
 
     cy.get('input[name=name]').type('Master Cypress')
-
     cy.get('select[name=category]').select('Personal time')
-
     cy.get('input[name=hours]').type('2')
   })
 
-  it('clears all form fields after creation', () => {
+  it('submits the form', () => {
     cy.get('form button').click()
+  })
 
+  it('clears all form fields after creation', () => {
+    cy.get('input[name=name]').should('be.focused')
     cy.get('input[name="name"]').should('be.empty')
     cy.get('select[name="category"]').should('have.value', 'Choose category')
     cy.get('input[name="hours"]').should('be.empty')
+    cy.get('form button').should('disabled')
+  })
+
+  it('closes the form', () => {
+    cy.get('[data-cy="close-form"]').click()
   })
 })
