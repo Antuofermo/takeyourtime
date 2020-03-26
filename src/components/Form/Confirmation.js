28593b2ced7abcdc3d8cd2708e9143c52af95dbb
Ludmila Antuofermo <l.antuofermo@web.de>
2020-03-26 17:44:01 +0100

    Delete comments

--- src/components/Form/Confirmation.js
+++ src/components/Form/Confirmation.js
@@ -2,36 +2,8 @@ import React from 'react'
 import styled from 'styled-components/macro'
 
 export default function Confirmation() {
-  return (
-    // <Message>
-    <Success>Added successfully!</Success>
-    //   {/* <AddNew onClick={addNew}>Add new activity</AddNew>
-    //   <ShowCat>Show categories</ShowCat> */}
-    // {/* </Message> */}
-  )
+  return <Success>Added successfully!</Success>
 }
-
-// const AddNew = styled.button`
-//   cursor: pointer;
-// `
-// const ShowCat = styled.button`
-//   cursor: pointer;
-// `
-
-// const Message = styled.div`
-//   position: absolute;
-//   text-align: center;
-//   padding: 15px;
-//   visibility: visible;
-//   border: 2px solid var(--color-gold);
-//   border-radius: 50px;
-//   width: 300px;
-//   height: 48px;
-//   font-size: 15px;
-//   color: var(--color-gold);
-//   font-size: 18px;
-//   background-color: var(--color-white);
-// `
 
 const Success = styled.p`
   position: absolute;
